16f4ce75d44a94ef964db9ce2ee0ec7a65364db9
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-08-02 21:59:13 +0530

    till circuit breaker

---
+++ order-service/order-service/src/main/java/com/gaurang/microservice/order/config/RestClientConfig.java
@@ -0,0 +1,35 @@
+package com.gaurang.microservice.order.config;
+
+import com.gaurang.microservice.order.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient(){
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+//    RestClient customClient = RestClient.builder()
+//            .requestFactory(new HttpComponentsClientHttpRequestFactory())
+//            .messageConverters(converters -> converters.add(new MyCustomMessageConverter()))
+//            .baseUrl("https://example.com")
+//            .defaultUriVariables(Map.of("variable", "foo"))
+//            .defaultHeader("My-Header", "Foo")
+//            .requestInterceptor(myCustomInterceptor)
+//            .requestInitializer(myCustomInitializer)
+//            .build();
+}
