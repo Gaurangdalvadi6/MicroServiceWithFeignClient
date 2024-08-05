1f8ba0f72d3465ecbc3456843d30a276e4927173
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-08-05 19:18:47 +0530

    circuit breaker finished

--- order-service/order-service/src/main/java/com/gaurang/microservice/order/config/RestClientConfig.java
+++ order-service/order-service/src/main/java/com/gaurang/microservice/order/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.gaurang.microservice.order.config;
 
 import com.gaurang.microservice.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,18 +23,17 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
-//    RestClient customClient = RestClient.builder()
-//            .requestFactory(new HttpComponentsClientHttpRequestFactory())
-//            .messageConverters(converters -> converters.add(new MyCustomMessageConverter()))
-//            .baseUrl("https://example.com")
-//            .defaultUriVariables(Map.of("variable", "foo"))
-//            .defaultHeader("My-Header", "Foo")
-//            .requestInterceptor(myCustomInterceptor)
-//            .requestInitializer(myCustomInitializer)
-//            .build();
+
+    private ClientHttpRequestFactory getClientRequestFactory(){
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
+    }
 }
