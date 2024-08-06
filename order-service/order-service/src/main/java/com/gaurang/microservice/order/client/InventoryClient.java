7965144656d420528340b4d817d48851b6623543
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-08-06 21:44:46 +0530

    some kafka issue in notification-service

--- order-service/order-service/src/main/java/com/gaurang/microservice/order/client/InventoryClient.java
+++ order-service/order-service/src/main/java/com/gaurang/microservice/order/client/InventoryClient.java
@@ -2,14 +2,13 @@ package com.gaurang.microservice.order.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
-import lombok.extern.slf4j.Slf4j;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 
-@Slf4j
 public interface InventoryClient {
 
     Logger log = LoggerFactory.getLogger(InventoryClient.class);
--- order-service/order-service/src/main/java/com/gaurang/microservice/order/service/OrderService.java
+++ order-service/order-service/src/main/java/com/gaurang/microservice/order/service/OrderService.java
@@ -2,20 +2,26 @@ package com.gaurang.microservice.order.service;
 
 import com.gaurang.microservice.order.client.InventoryClient;
 import com.gaurang.microservice.order.dto.OrderRequest;
+import com.gaurang.microservice.order.event.OrderPlacedEvent;
 import com.gaurang.microservice.order.model.Order;
 import com.gaurang.microservice.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
     private final InventoryClient inventoryClient;
+
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -29,6 +35,12 @@ public class OrderService {
             order.setQuantity(orderRequest.quantity());
             // save order to OrderRepository
             orderRepository.save(order);
+
+            // send the message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),orderRequest.userDetails().email());
+            log.info("Start- Sending OrderPlacedEvent {} to Kafka topic order-placed",orderPlacedEvent);
+            kafkaTemplate.send("order-placed",orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed",orderPlacedEvent);
         }else {
             throw new RuntimeException("Product with skucode "+orderRequest.skuCode()+" is not in stock");
         }
